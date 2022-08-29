52bc35a76c1d6c9e41dfcca0a1e366d9dfe1426d
EC2 Default User <thater114@gmail.com>
2022-08-29 08:18:59 +0000

    npm installed AWS s3 sync is used npm express is installed npm express cannot be removed.. need to be solved

---
+++ work/server-program/test.js
@@ -0,0 +1,13 @@
+var number1 = 10;
+var number2 = 10;
+var result = number1 + number2;
+
+
+console.log( `${number1} + ${number2} = ${result}`);
+
+
+if (number1 > number2){
+     console.log(`${number1} > ${number2}`);
+    } else{
+    console.log(`${number1} < ${number2}`);
+ }
